63d09f4d9606f7616905544b10edfca360da5706
Mirenk <minikmwinsea@gmail.com>
2023-11-19 03:18:41 +0900

    [add] generate modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LineModule } from './line/line.module';
+import { UserModule } from './user/user.module';
+import { DriverModule } from './driver/driver.module';
 
 @Module({
-  imports: [],
+  imports: [LineModule, UserModule, DriverModule],
   controllers: [AppController],
   providers: [AppService],
 })

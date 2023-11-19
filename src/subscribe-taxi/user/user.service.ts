cd83a509fdae2327e3bfe28ff545030fec3498ff
Mirenk <minikmwinsea@gmail.com>
2023-11-19 17:12:17 +0900

    [add] impl reserve

--- src/subscribe-taxi/user/user.service.ts
+++ src/subscribe-taxi/user/user.service.ts
@@ -43,7 +43,9 @@ export class UserService {
   private async followHandler(replyToken: string) {
     await this.LineService.replyMessage(replyToken, {
       type: 'text',
-      text: '友達登録ありがとうございます！',
+      text: `友達登録ありがとうございます！
+タクシーを呼びたいとき，「タクシー呼んで」と話しかけてください。
+または，「ここからマックスバリュ中央店まで行きたい」のように話しかけてください。`,
     });
   }
 
@@ -58,10 +60,60 @@ export class UserService {
 
   private async textHandler(message: TextEventMessage, replyToken: string) {
     const now = new Date();
-    const text = message.text;
+    const text = message.text.replace(/\s+/g, '');
 
     const place = await this.getPlaceMock();
 
+    // reserve
+    const reserveregex =
+      /(?<pickupspot>.+)から(?<dropoffspot>.+)(ま|へ)(.*)(行き|行く)/;
+    const match = text.match(reserveregex);
+
+    if (match?.groups?.dropoffspot) {
+      const pickupspot = place.find((p) =>
+        p.keywords.includes(match.groups.pickupspot),
+      );
+      const dropoffspot = place.find((p) =>
+        p.keywords.includes(match.groups.dropoffspot),
+      );
+      console.log(dropoffspot);
+      if (!dropoffspot) {
+        return await this.failedMessage(replyToken);
+      }
+
+      switch (match.groups.pickupspot) {
+        case 'ご自宅':
+        case '自宅':
+        case '家':
+          match.groups.pickupspot = 'ご自宅';
+          break;
+        default:
+          match.groups.pickupspot = '現在地';
+          break;
+      }
+
+      const reply: Message = {
+        type: 'text',
+        text: 'お迎えに上がります。しばらくお待ちくださいませ。',
+      };
+
+      const reserveresult = {
+        pickupspot: pickupspot?.spot || match.groups.pickupspot,
+        pickupdate: now,
+        dropoffspot: dropoffspot.spot,
+        dropoffdate: dayjs(now).add(dropoffspot.eta, 'minute').toDate(),
+      };
+
+      const flexmessage =
+        await this.FlexmessageService.createReserveResultMessage(reserveresult);
+
+      return await this.LineService.replyMessage(replyToken, [
+        reply,
+        flexmessage,
+      ]);
+    }
+
+    // view place list
     if (text.includes('タクシー') && text.includes('呼')) {
       const reply: Message = {
         type: 'text',
@@ -72,7 +124,7 @@ export class UserService {
         place.map(async (p): Promise<TPlaceList> => {
           return {
             headercolor: p.headercolor,
-            pickupspot: 'ご自宅',
+            pickupspot: '現在地',
             pickupdate: now,
             dropoffspot: p.spot,
             dropoffdate: dayjs(now).add(p.eta, 'minute').toDate(),
@@ -87,12 +139,15 @@ export class UserService {
         flexmessage,
       ]);
     }
-    switch (message.text) {
-      case '予約結果':
-        return await this.viewReserveResultDemo(replyToken);
-      default:
-        return;
-    }
+
+    return await this.failedMessage(replyToken);
+  }
+
+  private async failedMessage(replyToken: string) {
+    return await this.LineService.replyMessage(replyToken, {
+      type: 'text',
+      text: 'ごめんなさい。もう一度お願いします。',
+    });
   }
 
   private async getPlaceMock() {
@@ -100,31 +155,21 @@ export class UserService {
       headercolor: '#FF6B6E',
       spot: '由利組合総合病院',
       eta: 6,
+      keywords: ['病院', '総合病院', '由利病院', '由利組合総合病院'],
     };
     const postoffice = {
       headercolor: '#FF6B6E',
       spot: '本荘郵便局',
       eta: 12,
+      keywords: ['郵便局', '本荘郵便局'],
     };
     const market = {
       headercolor: '#27ACB2',
       spot: 'マックスバリュ中央店',
       eta: 4,
+      keywords: ['スーパー', 'マックスバリュ', 'マックスバリュ中央店'],
     };
 
     return [hospital, market, postoffice];
-  }
-
-  private async viewReserveResultDemo(replyToken: string) {
-    const reserveresult = {
-      pickupspot: 'ご自宅',
-      pickupdate: new Date(),
-      dropoffspot: '組合病院',
-      dropoffdate: new Date(),
-    };
-    return await this.LineService.replyMessage(
-      replyToken,
-      await this.FlexmessageService.createReserveResultMessage(reserveresult),
-    );
   }
 }
